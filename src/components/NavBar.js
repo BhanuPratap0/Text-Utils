12d6146fdff55428b0f4e57faf55422960d76265
BhanuPratap0 <bhanu.pratap.singh.2020@ecajmer.ac.in>
2023-11-29 00:53:12 +0530

    sd

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,7 @@ import Mode from './Mode'
 import { Link } from 'react-router-dom'
 
 export default function NavBar(props) {
-  const colorRed=()=>{
-    document.body.style.backgroundColor='#872341'
-  }
-  const colorBrown=()=>{
-    document.body.style.backgroundColor='#6B240C'
-  }
-  const mode1=()=>{
-    props.setMode('#6B240C');
-  }
+ 
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
@@ -30,10 +22,6 @@ export default function NavBar(props) {
         </li>
       </ul>
     </div>
-    
-    <button class="btn mx-2" onClick={colorRed} style={{backgroundColor:'#872341',borderRadius:'0px',width:'2px'}} ></button>
-    <button class="btn mx-2" onClick={colorBrown} style={{backgroundColor:'#6B240C',borderRadius:'0px',width:'2px'}} ></button>
-
     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
       <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
       <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
